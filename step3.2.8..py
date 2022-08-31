14df472d731368acedcc42f5378fd14bc3aadaa5
Dmitriy <carl.popper@yahoo.com>
2022-08-31 14:39:52 +0300

    corrected output phrase

--- step3.2.8..py
+++ step3.2.8..py
@@ -1,2 +1,2 @@
 def test_input_text(expected_result, actual_result):
-    assert actual_result == expected_result,  f"Wrong language, got {catalog_text} instead of {expected_result}"  
+    assert actual_result == expected_result,  f"expected {expected_result}, got {actual_result}"
